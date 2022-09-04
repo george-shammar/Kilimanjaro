0aaefabc1597e1005ca9c7a3cbc9781c2fb97696
george-shammar <george.gbenle@outlook.com>
2022-09-04 22:10:35 +0100

    Add connect wallet button

--- src/views/index-sections/Main-Page.js
+++ src/views/index-sections/Main-Page.js
@@ -4,14 +4,33 @@ import React from "react";
 import "../../assets/css/custom.css"
 
 // reactstrap components
-import { Container } from "reactstrap";
+import {
+  Button,
+  Collapse,
+  NavbarBrand,
+  Navbar,
+  NavItem,
+  NavLink,
+  Nav,
+  Container
+} from "reactstrap";
+
 
 // core components
 
 function MainPage() {
   return (
     <>
-    
+      <NavItem>
+              <Button
+                className="btn-round"
+                color="danger"
+                href=""
+                target="_blank"
+              >
+                Connect Wallet
+              </Button>
+            </NavItem>
     </>
   );
 }
