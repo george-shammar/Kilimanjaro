b0fec8ee8671b4e26c2bb55b95ca7f63669cdd23
george-shammar <george.gbenle@outlook.com>
2022-09-04 22:04:37 +0100

    Edit main page

--- src/views/index-sections/Main-Page.js
+++ src/views/index-sections/Main-Page.js
@@ -11,44 +11,7 @@ import { Container } from "reactstrap";
 function MainPage() {
   return (
     <>
-      <div
-        className="page-header section-dark"
-        style={{
-          backgroundImage:
-            "url(" + require("../../assets/img/antoine-barres.jpg") + ")"
-        }}
-      >
-        <div className="filter" />
-        <div className="content-center">
-          <Container>
-            
-            <div className="title-brand">
-            
-            <h2 className="text-center">
-            Regenerative Finance (ReFi)
-            </h2>
-            <h2 className="presentation-subtitle text-center">
-            with
-            </h2>
-              <h1 className="main-header"><span className="green">Kili</span>manjaro</h1>
-              <div className="fog-low">
-                <img alt="..." src={require("../../assets/img/fog-low.png")} />
-              </div>
-              <div className="fog-low right">
-                <img alt="..." src={require("../../assets/img/fog-low.png")} />
-              </div>
-            </div>
-            
-          </Container>
-        </div>
-        <div
-          className="moving-clouds"
-          style={{
-            backgroundImage: "url(" + require("../../assets/img/clouds.png") + ")"
-          }}
-        />
-        
-      </div>
+    
     </>
   );
 }
