77ed553cafd4ce1ea9b30be0e163ea4395e0ab99
george-shammar <george.gbenle@outlook.com>
2022-09-04 14:12:20 +0100

    Create DemoFooter page

---
+++ src/components/Footers/DemoFooter.js
@@ -0,0 +1,52 @@
+
+import React from "react";
+
+// reactstrap components
+import { Row, Container } from "reactstrap";
+
+function DemoFooter() {
+  return (
+    <footer className="footer footer-black footer-white">
+      <Container>
+        <Row>
+          <nav className="footer-nav">
+            <ul>
+              <li>
+                <a
+                  href=""
+                  target="_blank"
+                >
+                  Kilimanjaro
+                </a>
+              </li>
+              <li>
+                <a
+                  href=""
+                  target="_blank"
+                >
+                  Blog
+                </a>
+              </li>
+              <li>
+                <a
+                  href=""
+                  target="_blank"
+                >
+                  Licenses
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="credits ml-auto">
+            <span className="copyright">
+              © {new Date().getFullYear()}, made with{" "}
+              <i className="fa fa-heart heart" /> Kilimanjaro
+            </span>
+          </div>
+        </Row>
+      </Container>
+    </footer>
+  );
+}
+
+export default DemoFooter;
