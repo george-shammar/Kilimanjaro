db16dbc8f78aa2de1f66e5eb46cdc4f31e2b241d
george-shammar <george.gbenle@outlook.com>
2022-09-06 19:59:39 +0100

    Initialize counter

--- contracts/Kilimanjaro.sol
+++ contracts/Kilimanjaro.sol
@@ -9,5 +9,5 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract Kilimanjaro is ERC721URIStorage, Pausable, Ownable{
-
+  uint256 COUNTER;
 }
