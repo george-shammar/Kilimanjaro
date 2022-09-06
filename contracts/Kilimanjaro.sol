a760db0cb703a92dc6cdff88e72e7c2562704adb
george-shammar <george.gbenle@outlook.com>
2022-09-06 19:58:12 +0100

    Add openzeppelin SafeERC20 contract

--- contracts/Kilimanjaro.sol
+++ contracts/Kilimanjaro.sol
@@ -6,3 +6,4 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
