9b36d9a7c5908c730b718d40f9580be05dc0ec6d
george-shammar <george.gbenle@outlook.com>
2022-09-12 10:59:50 +0100

    Import helper components

--- src/views/index-sections/Main-Page.js
+++ src/views/index-sections/Main-Page.js
@@ -3,10 +3,20 @@
 import React from "react";
 import IndexNavbar from "../../components/Navbars/IndexNavbar";
 import { Link } from 'react-router-dom';
+import classnames from "classnames";
 import "../../assets/css/custom.css"
 
 // reactstrap components
-
+import {
+  Button,
+  Collapse,
+  NavbarBrand,
+  Navbar,
+  NavItem,
+  NavLink,
+  Nav,
+  Container
+} from "reactstrap";
 
 
 // core components
