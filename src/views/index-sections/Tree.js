4936c8c569742b8b37bcbec86453ab7f286c123d
george-shammar <george.gbenle@outlook.com>
2022-09-13 00:36:14 +0100

    Add provider

--- src/views/index-sections/Tree.js
+++ src/views/index-sections/Tree.js
@@ -15,6 +15,8 @@ function Tree() {
         setStatus("Input coordinate");
       }
 
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
 
 
   }
