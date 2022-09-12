e679575ed8c2c6c96c900d2a67d3c242bcd5ff14
george-shammar <george.gbenle@outlook.com>
2022-09-13 00:29:22 +0100

    Include error code

--- src/views/index-sections/Tree.js
+++ src/views/index-sections/Tree.js
@@ -3,6 +3,7 @@ import { ethers } from "ethers";
 import { NFTStorage, File } from 'nft.storage';
 
 const NFT_STORAGE_KEY = 'NFT_STORAGE_KEY';
+const ERROR_CODE_TX_REJECTED_BY_USER = 4001;
 
 function Tree() {
   return (
