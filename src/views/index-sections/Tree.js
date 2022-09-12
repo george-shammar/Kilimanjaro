f8341d60f666a7b562f804db5a0380b3dd4e351e
george-shammar <george.gbenle@outlook.com>
2022-09-13 00:28:41 +0100

    Add NFT_STORAGE_KEY

--- src/views/index-sections/Tree.js
+++ src/views/index-sections/Tree.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import { NFTStorage, File } from 'nft.storage';
 
+const NFT_STORAGE_KEY = 'NFT_STORAGE_KEY';
+
 function Tree() {
   return (
     <div>
