7311d445da8a4ad92473569a3b4935ad674b7145
george-shammar <george.gbenle@outlook.com>
2022-09-12 11:01:42 +0100

    Create Navbar

--- src/views/index-sections/Main-Page.js
+++ src/views/index-sections/Main-Page.js
@@ -1,7 +1,6 @@
 
 /*eslint-disable*/
 import React from "react";
-import IndexNavbar from "../../components/Navbars/IndexNavbar";
 import { Link } from 'react-router-dom';
 import classnames from "classnames";
 import "../../assets/css/custom.css"
@@ -23,6 +22,133 @@ import {
 
 function MainPage() {
 
+  const [navbarColor, setNavbarColor] = React.useState("navbar-transparent");
+  const [navbarCollapse, setNavbarCollapse] = React.useState(false);
+
+  const toggleNavbarCollapse = () => {
+    setNavbarCollapse(!navbarCollapse);
+    document.documentElement.classList.toggle("nav-open");
+  };
+
+  React.useEffect(() => {
+    const updateNavbarColor = () => {
+      if (
+        document.documentElement.scrollTop > 299 ||
+        document.body.scrollTop > 299
+      ) {
+        setNavbarColor("");
+      } else if (
+        document.documentElement.scrollTop < 300 ||
+        document.body.scrollTop < 300
+      ) {
+        setNavbarColor("navbar-transparent");
+      }
+    };
+
+    window.addEventListener("scroll", updateNavbarColor);
+
+    return function cleanup() {
+      window.removeEventListener("scroll", updateNavbarColor);
+    };
+  });
+
+
+  return (
+    <Navbar className={classnames("fixed-top", navbarColor)} expand="lg">
+      <Container>
+        <div className="navbar-translate">
+          <NavbarBrand
+            data-placement="bottom"
+            href="/index"
+            title="ReFi"
+          >
+            Kilimanjaro
+          </NavbarBrand>
+          <button
+            aria-expanded={navbarCollapse}
+            className={classnames("navbar-toggler navbar-toggler", {
+              toggled: navbarCollapse
+            })}
+            onClick={toggleNavbarCollapse}
+          >
+            <span className="navbar-toggler-bar bar1" />
+            <span className="navbar-toggler-bar bar2" />
+            <span className="navbar-toggler-bar bar3" />
+          </button>
+        </div>
+        <Collapse
+          className="justify-content-end"
+          navbar
+          isOpen={navbarCollapse}
+        >
+          <Nav navbar>
+            <NavItem>
+              <NavLink
+                data-placement="bottom"
+                href=""
+                target="_blank"
+                title="Follow us on Twitter"
+              >
+                <i className="fa fa-twitter" />
+                <p className="d-lg-none">Twitter</p>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                data-placement="bottom"
+                href=""
+                target="_blank"
+                title="Like us on Facebook"
+              >
+                <i className="fa fa-facebook-square" />
+                <p className="d-lg-none">Facebook</p>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                data-placement="bottom"
+                href=""
+                target="_blank"
+                title="Follow us on Instagram"
+              >
+                <i className="fa fa-instagram" />
+                <p className="d-lg-none">Instagram</p>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                data-placement="bottom"
+                href=""
+                target="_blank"
+                title="Star on GitHub"
+              >
+                <i className="fa fa-github" />
+                <p className="d-lg-none">GitHub</p>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                href=""
+                target="_blank"
+              >
+                <i className="nc-icon nc-book-bookmark" /> DOCS
+              </NavLink>
+            </NavItem>
+            <Link to="/MainPage"><NavItem>
+              <Button
+                className="btn-round"
+                color="danger"
+                href=""
+                target="_blank"
+              >
+                Get Started
+              </Button>
+            </NavItem></Link>
+          </Nav>
+        </Collapse>
+      </Container>
+    </Navbar>
+  );
   
   
 }
