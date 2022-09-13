95c3bffb2374e8e095d0f49ab953c1c1531571fd
george-shammar <george.gbenle@outlook.com>
2022-09-13 20:43:59 +0100

    updateSpeciesInput

--- src/views/index-sections/Tree.js
+++ src/views/index-sections/Tree.js
@@ -68,6 +68,30 @@ function Tree() {
 
   return (
     <div>
+
+
+      <div>
+      
+      <div className="border">
+        <input 
+          className="py-1" 
+          placeholder="Species"
+          required
+          onChange={e => updateSpeciesInput({...speciesInput, name: e.target.value})}  
+        />
+        <div className="white mt-3">
+          <p>ID: To be revealed</p>
+          <p>DNA: To be revealed</p>
+          <p>Rarity: To be revealed</p>
+          <p>Level: To be revealed</p>
+          <p>Description/Powers: To be reveealed</p>
+        </div>
+          <button className="py-2 submit white" onClick={mintMage}>
+              Mint Mage
+          </button>
+        </div>
+
+      </div>
       <button>
         Plant Tree
       </button>
